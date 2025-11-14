384e0bcbd82dfdfab206388ace1c2caf9b3497b0
kashikakochar11 <kashikakochar11@gmail.com>
2025-11-14 23:57:30 +0530

    Add files via upload

---
+++ EXPERIMENT 6/Q3.c
@@ -0,0 +1,35 @@
+/*Develop a recursive function FIBO (num) that accepts an integer argument.
+Write a C program that invokes this function to generate the Fibonacci
+sequence up to num*/
+#include <stdio.h>
+
+int factorial(int n) {
+   //base case
+   if(n == 0) {
+      return 1;
+   } else {
+      return n * factorial(n-1);
+   }
+}
+
+int fibbonacci(int n) {
+   if(n == 0){
+      return 0;
+   } else if(n == 1) {
+      return 1;
+   } else {
+      return (fibbonacci(n-1) + fibbonacci(n-2));
+   }
+}
+
+int main() {
+   int n = 5;
+   int i;
+	
+   printf("Factorial of %d: %d\n" , n , factorial(n));
+   printf("Fibbonacci of %d: " , n);
+	
+   for(i = 0;i<n;i++) {
+      printf("%d ",fibbonacci(i));            
+   }
+}
